3c704f3c3371d12041146f846bd678f7bcd3e752
Suba Sah <sah.suba@gmail.com>
2021-06-12 16:11:54 -0400

    changes in the file

--- volttron/platform/web/vui_endpoints.py
+++ volttron/platform/web/vui_endpoints.py
@@ -16,7 +16,7 @@ from services.core.ActuatorAgent.actuator.agent import LockError
 
 from volttron.platform.web.topic_tree import DeviceTree
 
-# his there
+# his thered
 
 import logging
 _log = logging.getLogger(__name__)
